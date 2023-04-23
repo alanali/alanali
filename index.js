9771846e590e8aa8c182b1ebfba2cf7b92b4920a
Alana Li <ruiruialana@gmail.com>
2023-04-23 01:22:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 async function main() {
     const readmeTemplate = (
-        await fs.readFile(path.join(process.cwd(), "./README.md"))
+        await fs.readFile(path.join(process.cwd(), "./README.template.md"))
     ).toString("utf-8");
 
     const office_quote = await (
