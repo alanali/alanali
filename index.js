9121230ac6fa49c0ce179ab9db2081bafcc0d7b3
Alana Li <ruiruialana@gmail.com>
2023-04-23 01:10:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function main() {
 
 
     const readme = readmeTemplate
-        .replace("{office_quote}", office_quote.character)
+        .replace("{office_quote}", office_quote.quote)
         .replace("{office_character}", `- ${office_quote.character}`)
 
     await fs.writeFile("README.md", readme);
